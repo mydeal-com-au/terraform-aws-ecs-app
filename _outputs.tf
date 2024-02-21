6e7f22df560658588b6611c8bb851017c06f200e
matthosking-mydeal <95453715+matthosking-mydeal@users.noreply.github.com>
2024-02-21 16:21:30 +1100

    add additional outputs for use with scheduler

--- _outputs.tf
+++ _outputs.tf
@@ -1,3 +1,15 @@
 output "aws_cloudwatch_log_group_arn" {
   value = aws_cloudwatch_log_group.default.arn
 }
+
+output "task_definition_arn" {
+  value = aws_ecs_task_definition.default.arn
+}
+
+output "task_definition_arn_without_revision" {
+  value = aws_ecs_task_definition.default.arn_without_revision
+}
+
+output "task_definition_revision" {
+  value = aws_ecs_task_definition.default.revision
+}
