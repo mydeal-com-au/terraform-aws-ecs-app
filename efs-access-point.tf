74c0d8ffdbcf76bbe1e0b9faaf0c772bd83ff599
matthosking-mydeal <95453715+matthosking-mydeal@users.noreply.github.com>
2024-08-25 16:35:38 +1000

    fix key for efs access points

--- efs-access-point.tf
+++ efs-access-point.tf
@@ -1,6 +1,6 @@
 resource "aws_efs_access_point" "default" {
-  for_each       = var.efs_mapping
-  file_system_id = each.value.file_system_id
+  for_each       = { for mapping in var.efs_mapping : mapping.file_system_id => mapping }
+  file_system_id = each.key
   root_directory {
     creation_info {
       owner_gid   = 0
