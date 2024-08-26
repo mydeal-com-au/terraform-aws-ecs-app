11ef99b637911e8e668e153c682b63d03070f32a
matthosking-mydeal <95453715+matthosking-mydeal@users.noreply.github.com>
2024-08-26 17:51:02 +1000

    Update efs-access-point.tf

--- efs-access-point.tf
+++ efs-access-point.tf
@@ -1,5 +1,5 @@
 resource "aws_efs_access_point" "default" {
-  for_each       = { for mapping in var.efs_mapping : "${mapping.file_system_id}${mapping.file_system_path}" => mapping }
+  for_each       = { for mapping in var.efs_mapping : "${mapping.file_system_id}${replace(mapping.file_system_path, "/", "-")}" => mapping }
   file_system_id = each.value.file_system_id
   root_directory {
     creation_info {
